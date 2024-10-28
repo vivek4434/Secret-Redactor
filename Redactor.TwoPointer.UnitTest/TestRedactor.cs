41e83d633325ce8d647236f8ffdc291bf2588a2b
Vivek Upadhyay <viupadh@microsoft.com>
2024-10-28 12:25:23 +0530

    Removing unused

--- Redactor.TwoPointer.UnitTest/TestRedactor.cs
+++ Redactor.TwoPointer.UnitTest/TestRedactor.cs
@@ -7,7 +7,6 @@
     [TestClass]
     public class TestRedactor
     {
-        private const string TEST_INPUT = "TEST_INPUT";
         private const string TEST_COMMAND_1 = " key 123400000000000000000000000000000000000000000000abc\n\n\n\n\n\n\nRP/0/RP0/CPU(config-test-primary-test-64309)#";
         private readonly IEnumerable<string> TEST_LOOKUP_SET = new List<string>() { "TEST_SECRET_1", "TEST_SECRET_2", "123400000000000000000000000000000000000000000000abc", "12340000678", "ion", "678", "o9-*t&p()(" };
         private const string TEST_COMMAND = "This version\n\n\n\n versions is a TEST_SECRET_1";
@@ -19,7 +18,6 @@
         private const string TEST_SECRET_3 = "123400000000000000000000000000000000000000000000abc";
         private const string TEST_SECRET_4 = "678";
         private const string TEST_SECRET_5 = "o9-*t&p()(";
-        private const string TEST_WHITELIST_STRING = "oneops00";
 
         [TestMethod]
         public void TestRedactSuccess()
